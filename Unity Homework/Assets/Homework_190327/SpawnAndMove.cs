0fa237d83c3459295cb917ff70654c5dd2cf9f5f
teachgame <teachgame@163.com>
2019-03-28 15:24:22 +0800

    将 C# 和 Unity 作业合并到一个代码库。参考答案 03.26 03.27 #7 #8

---
+++ Unity Homework/Assets/Homework_190327/SpawnAndMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAndMove : MonoBehaviour
+{
+    private GameObject playerPrefab;
+    private GameObject player;
+
+    void Start()
+    {
+        playerPrefab =  Resources.Load<GameObject>("Player");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            player = (GameObject)Instantiate(playerPrefab, Vector3.up, Quaternion.identity);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            player.transform.position += player.transform.forward * 10 * Time.deltaTime;
+        }
+    }
+}
