266d40786f70d5a34ba72c0e327073f95ebddda1
teachgame <teachgame@163.com>
2019-04-01 13:59:18 +0800

    参考答案 19.03.29

---
+++ Unity_Homework/Assets/Homework_190329/GameController_CollectItem.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController_CollectItem : MonoBehaviour
+{
+    public int collectableCounts = 10;
+    public float pickupRadius = 5;
+
+    public Material pickupMat;
+    public Material normalMat;
+
+    private int score;
+
+    private GameObject playerPrefab;
+    private GameObject collectablePrefab;
+
+    private GameObject player;
+    private List<GameObject> collectables = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPrefab = Resources.Load<GameObject>("Player");
+        collectablePrefab = Resources.Load<GameObject>("Collectable");
+
+        SpawnPlayer();
+        SpawnCollectables();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pickupRadius = Mathf.Sin(Time.time) * 2 + 4;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (player != null)
+            {
+                player.transform.position += player.transform.forward * 10 * Time.deltaTime;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (player != null)
+            {
+                player.transform.position -= player.transform.forward * 10 * Time.deltaTime;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (player != null)
+            {
+                player.transform.Rotate(0, -90 * Time.deltaTime, 0);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (player != null)
+            {
+                player.transform.Rotate(0, 90 * Time.deltaTime, 0);
+            }
+        }
+
+        CheckPickup();
+    }
+
+    void SpawnPlayer()
+    {
+        player = Instantiate(playerPrefab, Vector3.up, Quaternion.identity);
+    }
+
+    void SpawnCollectables()
+    {
+        for (int i = 0; i < collectableCounts; i++)
+        {
+            Vector2 randomCircle = Random.insideUnitCircle * 25;
+            Vector3 spawnPos = new Vector3(randomCircle.x, 1, randomCircle.y);
+
+            GameObject collectable = Instantiate(collectablePrefab, spawnPos, Quaternion.identity);
+
+            collectables.Add(collectable);
+        }
+    }
+
+    void CheckPickup()
+    {
+        bool pickup = Input.GetKeyDown(KeyCode.Space);
+
+        float sqrPickupRaidus = pickupRadius * pickupRadius;
+
+        for (int i = 0; i < collectables.Count; i++)
+        {
+            Vector3 collectable_to_player = player.transform.position - collectables[i].transform.position;
+            float sqrMag = collectable_to_player.sqrMagnitude;
+
+            if( sqrMag < sqrPickupRaidus)
+            {
+                if(pickup)
+                {
+                    if(collectables[i].activeSelf)
+                    {
+                        collectables[i].SetActive(false);
+                        AddScore();
+                    }
+                }
+                else
+                {
+                    collectables[i].GetComponent<MeshRenderer>().material = pickupMat;
+                }
+            }
+            else
+            {
+                collectables[i].GetComponent<MeshRenderer>().material = normalMat;
+            }
+        }
+    }
+
+    void AddScore()
+    {
+        score += 100;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(player != null)
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(player.transform.position, pickupRadius);
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.BeginVertical();
+        GUILayout.Label(string.Format("得分：{0}", score));
+        GUILayout.EndVertical();
+    }
+}
---
+++ Unity_Homework/Assets/Homework_190329/VectorTest.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorTest : MonoBehaviour
+{
+    public float xAxisLength = 10;
+    public float yAxisLength = 10;
+
+    public float pointSize = 0.1f;
+
+    public Vector3 a;
+    public Vector3 b;
+    public Vector3 c;
+
+    public Vector3 movePoint;
+
+    public Color colorA;
+    public Color colorB; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 ab = b - a;
+        Debug.Log(string.Format("点A到点B的距离是{0}", ab.magnitude));
+
+        Vector3 co = c - Vector3.zero;
+        Debug.Log(string.Format("点C到原点的距离是{0}", co.magnitude));
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            movePoint -= Vector3.right;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            movePoint += Vector3.right;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            movePoint += Vector3.up;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            movePoint -= Vector3.up;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawSphere(Vector3.zero, pointSize);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(Vector3.zero, Vector3.right * xAxisLength);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(Vector3.zero, Vector3.up * yAxisLength);
+
+        Gizmos.color = colorA;
+        Gizmos.DrawSphere(a, pointSize);
+
+        Gizmos.color = colorB;
+        Gizmos.DrawSphere(b, pointSize);
+
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawSphere(c, pointSize);
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(a, b);
+        Gizmos.DrawLine(c, Vector3.zero);
+
+        Gizmos.color = Color.white;
+        Vector3 ab = b - a;
+        Gizmos.DrawLine(movePoint, movePoint + ab);
+
+    }
+}
