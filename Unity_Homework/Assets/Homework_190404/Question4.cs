4e27c789466b35752365d394cb7490906685c740
teachgame <teachgame@163.com>
2019-04-05 12:26:15 +0800

    参考答案 190403 #13

---
+++ Unity_Homework/Assets/Homework_190404/Question4.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question4 : MonoBehaviour
+{
+    private Vector3 fPos;
+    private List<Vector3> posList = new List<Vector3>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float f = Mathf.Sin(2 * Mathf.PI * Time.time) * 5 + 5;
+        fPos = Vector3.right * Time.time + Vector3.up * f;
+        posList.Add(fPos);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+
+        for(int i = 0; i < posList.Count; i++)
+        {
+            Gizmos.DrawSphere(posList[i], 0.1f);
+        }
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(Vector3.zero, Vector3.right * 20);
+        Gizmos.DrawLine(Vector3.zero, Vector3.up * 20);
+    }
+}
---
+++ Unity_Homework/Assets/Homework_190404/Question5.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question5 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 direction = new Vector3(1, -1, 0);
+        direction.Normalize();
+
+        float speed = 10f;
+
+        Vector3 posAfter1Sec = direction * speed;
+
+        transform.Translate(direction * speed * Time.deltaTime, Space.World);
+
+        if(transform.position.sqrMagnitude > 50 * 50)
+        {
+            transform.position = Vector3.zero;
+        }
+
+        Gizmos.DrawSphere(transform.position, 1f);
+        Gizmos.DrawSphere(transform.position.x * Vector3.right, 1f);
+        Gizmos.DrawSphere(transform.position.y * Vector3.up, 1f);
+
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawSphere(posAfter1Sec, 1f);
+        Debug.Log("1秒后的位置:" + posAfter1Sec);
+    }
+}
